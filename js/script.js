d35f94f4b1e96ef3d5cc8f22a2eebbe6ec6a45bd
vigoraj <govindaraj.k@capgemini.com>
2024-07-02 16:35:24 +0530

    dashboard design

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// import userIcon from "./images";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("loginForm");
+  const displayname = document.getElementById("displayname");
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const userInput = document.getElementById("username");
+    const passInput = document.getElementById("password");
+
+    if (userInput.value !== "" && passInput.value !== "") {
+      localStorage.setItem("Name", userInput.value);
+      window.location.href = "dashboard.html";
+    } else {
+      alert("Username / Password is Required!");
+      userInput.focus();
+    }
+  });
+
+  // console.log(userIcon);
+});
+
+const user = localStorage.getItem("Name");
+const checkbox = document.getElementById("rememberMe");
+
+
+
+// const isChecked = checkbox.checked;
+
+displayname.innerHTML = user;
+
+const logout = () => {
+  localStorage.removeItem("Name");
+  window.location.href = "index.html";
+};
+
+// const sidebarCollapse = () => {
+//   const collapseBtn = document.getElementById("sidebar-collapse");
+//   collapseBtn.classList.add("hide");
+// };
